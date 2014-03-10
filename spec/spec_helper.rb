f29af1dca5f9175858aea556efdb9ba24901fa85
Brian Cardarella <bcardarella@gmail.com>
2014-03-10 11:01:38 -0400

    Cleaned up Deprecation Warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,5 +15,6 @@ Dir[File.join(ENGINE_RAILS_ROOT, 'spec/config/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.mock_with :mocha
   config.include Factory::Syntax::Methods
-  config.backtrace_clean_patterns = []
 end
+
+I18n.enforce_available_locales = true
